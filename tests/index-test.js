00614da2569d519e30fdb986199a6ad38253fc81
matthewhudson <matthud@gmail.com>
2017-12-06 14:39:35 -0500

    Improving test coverage.

--- tests/index-test.js
+++ tests/index-test.js
@@ -1,77 +1,57 @@
-import expect from 'expect'
-import kash from 'src'
+import expect from 'expect';
+import kash from 'src';
 
 describe('kash', () => {
+  afterEach(() => {
+    kash.flush();
+  });
 
-  it('Setting a single cache item', () => {
-
-    kash.set('foo', 'bar')
+  describe('#set', () => {
+    beforeEach(() => {
+      kash.set('foo', 'bar');
+    });
 
     it('should have a value of `bar`', () => {
-      expect(kash.get('foo')).toEqual('bar')
-    })
+      expect(kash.get('foo')).toEqual('bar');
+    });
 
     it('should have a size of 1', () => {
-      expect(kash.size()).toEqual(1)
-    })
+      expect(kash.size()).toEqual(1);
+    });
+  });
 
-  })
-
-  it('Checking the cache after a key expires', () => {
-
-    kash.flush()
-    kash.set('foo', 'bar', 100)
-
-    it('should have a value of `null`', () => {
-      expect(kash.get('foo')).toEqual(null)
-    })
-
-    it('should have a size of 0', () => {
-      expect(kash.size()).toEqual(0)
-    })
-
-  })
-
-  it('Checking the cache size', () => {
-
-    kash.flush()
-
-    let i = 0
-    while (i < 8) {
-      kash.set(i.toString(), i.toString())
-      i++
-    }
+  describe('#size', () => {
+    beforeEach(() => {
+      let i = 0;
+      while (i < 8) {
+        kash.set(i.toString(), i.toString());
+        i++;
+      }
+    });
 
     it('should have a size of 8', () => {
-      expect(kash.size()).toEqual(8)
-    })
+      expect(kash.size()).toEqual(8);
+    });
+  });
 
-  })
-
-  it('Flushing the cache', () => {
-
-    kash.flush()
-
+  describe('#flush', () => {
     it('should have a size of 0', () => {
-      expect(kash.size()).toEqual(0)
-    })
+      expect(kash.size()).toEqual(0);
+    });
+  });
 
-  })
-
-  it('Deleting a key', () => {
-
-    kash.flush()
-    kash.set('foo', 'bar')
-    kash.del('foo')
+  describe('#del', () => {
+    beforeEach(() => {
+      kash.set('foo', 'bar');
+      kash.del('foo');
+    });
 
     it('should have a value of `null`', () => {
-      expect(kash.get('foo')).toEqual(null)
-    })
+      expect(kash.get('foo')).toEqual(null);
+    });
 
     it('should have a size of 0', () => {
-      expect(kash.size()).toEqual(0)
-    })
-
-  })
-
-})
+      expect(kash.size()).toEqual(0);
+    });
+  });
+});
